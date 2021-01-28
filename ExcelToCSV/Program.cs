b381816332c736609b84648b2673ee39b90e5de1
avajang-pc <avajang-pc@DESKTOP-L3O4AE9>
2021-01-28 20:18:12 +0330

    change for getting is have header

--- ExcelToCSV/Program.cs
+++ ExcelToCSV/Program.cs
@@ -15,12 +15,21 @@ namespace ExcelToCSV
         {
             if (args.Length < 2)
             {
-                Console.WriteLine("please add srouce excel and destination csv file like (ExcelToCSV test.excel test.csv)");
+                Console.WriteLine("please add srouce excel and destination csv file like " +
+                    "(ExcelToCSV test.xls test.csv [true])" +
+                    "\r\n test excel file (extention xls or xlsx)" +
+                    "\r\n  test csv file" +                    
+                    "\r\n optional [true/false] for is have header or not");
                 return -1;
             }
             try
             {
                 var filePath = args[0];
+                var isHaveHeader = false;
+                if (args.Length > 2)
+                {
+                    bool.TryParse(args[2], out isHaveHeader);
+                }
                 FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
 
                 //Choose one of either 1 or 2
@@ -35,10 +44,10 @@ namespace ExcelToCSV
                     //2. Reading from a OpenXml Excel file (2007 format; *.xlsx)
                     excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
                 }
-                excelReader.IsFirstRowAsColumnNames = true;
+                excelReader.IsFirstRowAsColumnNames = isHaveHeader;
                 DataSet result = excelReader.AsDataSet();
                 var datatable = result.Tables[0];
-                ToCSV(datatable, args[1]);
+                ToCSV(isHaveHeader, datatable, args[1]);
                 Console.WriteLine("Make {0} successfully", args[1]);
             }
             catch (Exception ex)
@@ -48,19 +57,22 @@ namespace ExcelToCSV
             return 0;
         }
 
-        private static void ToCSV(DataTable dtDataTable, string strFilePath)
+        private static void ToCSV(bool isHaveHeader, DataTable dtDataTable, string strFilePath)
         {
             StreamWriter sw = new StreamWriter(strFilePath, false);
             //headers    
-            for (int i = 0; i < dtDataTable.Columns.Count; i++)
+            if (isHaveHeader)
             {
-                sw.Write(dtDataTable.Columns[i]);
-                if (i < dtDataTable.Columns.Count - 1)
+                for (int i = 0; i < dtDataTable.Columns.Count; i++)
                 {
-                    sw.Write(",");
+                    sw.Write(dtDataTable.Columns[i]);
+                    if (i < dtDataTable.Columns.Count - 1)
+                    {
+                        sw.Write(",");
+                    }
                 }
+                sw.Write(sw.NewLine);
             }
-            sw.Write(sw.NewLine);
             foreach (DataRow dr in dtDataTable.Rows)
             {
                 for (int i = 0; i < dtDataTable.Columns.Count; i++)
@@ -70,7 +82,7 @@ namespace ExcelToCSV
                         string value = dr[i].ToString();
                         if (value.Contains(','))
                         {
-                            value = string.Format("\"{0}\"", value.Replace(',', ' '));
+                            value = string.Format("\"{0}\"", i == 0 ? value.Replace(',', '_') : value);
                             sw.Write(value);
                         }
                         else
